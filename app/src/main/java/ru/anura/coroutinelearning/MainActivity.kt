0d18e617427f5740b98fe342bc0523644f632d1c
anura1402 <ann99909@gmail.com>
2024-06-19 21:44:12 +0300

    Two methods of using posting messages to mainThread. Handler, Looper and runOnUiThread

--- app/src/main/java/ru/anura/coroutinelearning/MainActivity.kt
+++ app/src/main/java/ru/anura/coroutinelearning/MainActivity.kt
@@ -1,6 +1,8 @@
 package ru.anura.coroutinelearning
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -13,6 +15,7 @@ class MainActivity : AppCompatActivity() {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
@@ -24,7 +27,7 @@ class MainActivity : AppCompatActivity() {
     private fun loadData() {
         binding.progress.isVisible = true
         binding.buttonLoad.isEnabled = false
-        loadCity(){
+        loadCity() {
             binding.tvLocation.text = it
             loadTemperature(it) {
                 binding.tvTemperature.text = it.toString()
@@ -34,22 +37,28 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun loadCity(callback: (String)->Unit) {
+    private fun loadCity(callback: (String) -> Unit) {
         thread {
             Thread.sleep(5000)
-            callback.invoke("Moscow")
+            Handler(Looper.getMainLooper()).post {
+                callback.invoke("Moscow")
+            }
         }
     }
 
     private fun loadTemperature(city: String, callback: (Int) -> Unit) {
         thread {
-            Toast.makeText(
-                this,
-                getString(R.string.loading_temperature_toast, city),
-                Toast.LENGTH_SHORT
-            ).show()
+            runOnUiThread {
+                Toast.makeText(
+                    this,
+                    getString(R.string.loading_temperature_toast, city),
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
             Thread.sleep(5000)
-            callback.invoke(17)
+            runOnUiThread {
+                callback.invoke(17)
+            }
         }
 
     }
