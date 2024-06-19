61382aed2fa11ed49eb1649e7a6c3bf6c8052caa
anura1402 <ann99909@gmail.com>
2024-06-19 21:15:41 +0300

    Synchronous app

--- app/src/main/java/ru/anura/coroutinelearning/MainActivity.kt
+++ app/src/main/java/ru/anura/coroutinelearning/MainActivity.kt
@@ -1,20 +1,49 @@
 package ru.anura.coroutinelearning
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import ru.anura.coroutinelearning.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        setContentView(binding.root)
+        binding.buttonLoad.setOnClickListener {
+            loadData()
         }
+    }
+
+    private fun loadData() {
+        binding.progress.isVisible = true
+        binding.buttonLoad.isEnabled = false
+        val city = loadCity()
+        binding.tvLocation.text = city
+        val temp = loadTemperature(city)
+        binding.tvTemperature.text = temp.toString()
+        binding.progress.isVisible = false
+        binding.buttonLoad.isEnabled = true
+    }
+
+    private fun loadCity(): String {
+
+        Thread.sleep(5000)
+        return "Moscow"
+    }
+
+    private fun loadTemperature(city: String): Int {
+        Toast.makeText(
+            this,
+            getString(R.string.loading_temperature_toast, city),
+            Toast.LENGTH_SHORT
+        ).show()
+        Thread.sleep(5000)
+        return 17
     }
 }
